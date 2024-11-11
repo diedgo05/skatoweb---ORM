e6337d50792516fff489a62df236408356e7a1a4
diedgo05 <233358@ids.upchiapas.edu.mx>
2024-11-11 01:20:28 -0600

    API terminada

--- routes/guide.js
+++ routes/guide.js
@@ -7,6 +7,6 @@ router.get("/", guideController.getAllGuides);
 router.get("/:idUser", guideController.getGuideByID);
 router.put("/up/:id", guideController.updateGuide);
 router.delete("/delete/:id", guideController.deleteGuide);
-// router.get("/search", guideController.searchGuidesByTrickName);
+    // router.get("/search", guideController.searchGuidesByTrickName);
 
 module.exports = router;
