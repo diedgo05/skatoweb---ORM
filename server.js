21ab9b88ed65c8fea26aa404e6927aeb3221359a
diedgo05 <233358@ids.upchiapas.edu.mx>
2026-05-27 04:56:13 -0600

    fix render deploy

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const http = require("http");
 const app = require("./app");
 const { connection } = require("./models");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 connection
-  .sync() // Sincroniza los modelos con la base de datos
+  .sync()
   .then(() => {
     console.log(
       "Conexión con la base de datos exitosa y modelos sincronizados."
     );
+
     const server = http.createServer(app);
+
     server.listen(PORT, () => {
       console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
